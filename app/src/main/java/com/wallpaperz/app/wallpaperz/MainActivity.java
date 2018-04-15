0fb789357d802e29e8a0fd01b4cc2a507aca980e
Khan Sharukh <khan_sharukh@outlook.com>
2018-04-15 12:30:27 +0530

    add fab buttons - set wallpaper and download

--- app/src/main/java/com/wallpaperz/app/wallpaperz/MainActivity.java
+++ app/src/main/java/com/wallpaperz/app/wallpaperz/MainActivity.java
@@ -6,20 +6,20 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -29,14 +29,17 @@ import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     ImageView imageView;
+    FloatingActionButton fabSetWallpaper, fabDownload;
 
+    //TODO(1) - ADD TWO FLOATING BUTTONS - LEFT: DOWNLOAD ICON, RIGHT: CHECK ICON - https://developer.android.com/guide/topics/ui/floating-action-button.html
+    //TODO(2) - NASA API - https://api.nasa.gov/planetary/apod?api_key=tkgqOsB5B9xIhYzX4Nwwzip7QZ7UFrcpusQdMtGv&date=2018-04-12
+    //TODO(3) - PIXABAY API - https://pixabay.com/api/?key=8684158-90f55835045c8261b7130a08b&q=yellow+flowers&image_type=photo&pretty=true&per_page=3
+    //TODO(4) - ADD REFRESH BUTTON ON APP BAR RIGHT - PULL TO REFRESH
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +58,23 @@ public class MainActivity extends AppCompatActivity
 
         //volleyGetRequest();
 
+        fabSetWallpaper = findViewById(R.id.fabWallpaper);
+        fabDownload = findViewById(R.id.fabDownload);
+
+        fabSetWallpaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Wallpaper Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        fabDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Download Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         imageView = (ImageView) findViewById(R.id.iv_load_image);
         Picasso.get()
                 .load("https://source.unsplash.com/random")
